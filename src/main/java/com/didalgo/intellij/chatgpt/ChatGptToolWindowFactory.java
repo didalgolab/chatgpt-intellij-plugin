7b1872cf25663118f1243e59f7ecf00677889cb2
Mariusz Bernacki <consulting@didalgo.com>
2024-02-27 22:11:00 +0100

    Fix: Does not run on Android Studio Hedgehog (#15)
    Also, fixes: Cannot init toolwindow error under Linux (#13)

--- src/main/java/com/didalgo/intellij/chatgpt/ChatGptToolWindowFactory.java
+++ src/main/java/com/didalgo/intellij/chatgpt/ChatGptToolWindowFactory.java
@@ -10,6 +10,7 @@ import com.didalgo.intellij.chatgpt.settings.OpenAISettingsState;
 import com.didalgo.intellij.chatgpt.ui.action.tool.SettingsAction;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -25,6 +26,7 @@ import java.util.*;
 
 public class ChatGptToolWindowFactory implements ToolWindowFactory, DumbAware {
     public static final Key<ModelPage> ACTIVE_TAB = Key.create("didalgo.ChatGptToolWindow.ACTIVE_TAB");
+    private static final Logger log = Logger.getInstance(ChatGptToolWindowFactory.class);
 
     public static final String GPT35_TURBO_CONTENT_NAME = "GPT-3.5-Turbo";
     public static final String GPT4_CONTENT_NAME = "GPT-4";
@@ -46,24 +48,30 @@ public class ChatGptToolWindowFactory implements ToolWindowFactory, DumbAware {
         gpt4.putUserData(ACTIVE_TAB, ModelPage.GPT_4);
         gpt4.setCloseable(false);
 
-        BrowserContent browserToolWindow = new BrowserContent(project);
-        Content browser = contentFactory.createContent(browserToolWindow.getContentPanel(), ONLINE_CHATGPT_CONTENT_NAME, false);
-        browser.putUserData(ACTIVE_TAB, ModelPage.ONLINE);
-        browser.setCloseable(false);
-
         toolWindow.getContentManager().addContent(gpt35Turbo);
         toolWindow.getContentManager().addContent(gpt4);
-        toolWindow.getContentManager().addContent(browser);
+
+        BrowserContent browserToolWindow = null;
+        try {
+            browserToolWindow = new BrowserContent(project);
+            Content browser = contentFactory.createContent(browserToolWindow.getContentPanel(), ONLINE_CHATGPT_CONTENT_NAME, false);
+            browser.putUserData(ACTIVE_TAB, ModelPage.ONLINE);
+            browser.setCloseable(false);
+            toolWindow.getContentManager().addContent(browser);
+        } catch (IllegalStateException e) {
+            log.warn("'ChatGPT Online' is disabled due to: " + e.getMessage());
+        }
 
         // Set the default component. It require the 1st container
         ModelPage firstContent = ModelPage.of(PropertiesComponent.getInstance().getValue(ACTIVE_CONTENT_KEY, ModelPage.GPT_3_5.name()));
         switch (firstContent.name()) {
             case ModelPage.Of.GPT_3_5 -> project.putUserData(ChatLink.KEY, gpt35TurboToolWindow.getChatLink());
             case ModelPage.Of.GPT_4   -> project.putUserData(ChatLink.KEY, gpt4ToolWindow.getChatLink());
-            case ModelPage.Of.ONLINE  -> project.putUserData(ChatLink.KEY, browserToolWindow.getChatLink());
+            case ModelPage.Of.ONLINE  -> Optional.ofNullable(browserToolWindow).ifPresent(win -> project.putUserData(ChatLink.KEY, win.getChatLink()));
         }
 
         // Add the selection listener
+        var _browserToolWindow = browserToolWindow;
         toolWindow.addContentManagerListener(new ContentManagerListener() {
             @Override
             public void selectionChanged(@NotNull ContentManagerEvent event) {
@@ -72,7 +80,7 @@ public class ChatGptToolWindowFactory implements ToolWindowFactory, DumbAware {
                     switch (page.name()) {
                         case ModelPage.Of.GPT_3_5 -> project.putUserData(ChatLink.KEY, gpt35TurboToolWindow.getChatLink());
                         case ModelPage.Of.GPT_4   -> project.putUserData(ChatLink.KEY, gpt4ToolWindow.getChatLink());
-                        case ModelPage.Of.ONLINE  -> project.putUserData(ChatLink.KEY, browserToolWindow.getChatLink());
+                        case ModelPage.Of.ONLINE  -> Optional.ofNullable(_browserToolWindow).ifPresent(win -> project.putUserData(ChatLink.KEY, win.getChatLink()));
                     }
                 }
                 PropertiesComponent.getInstance(project).setValue(ACTIVE_CONTENT_KEY, (page == null)? null: page.name());
