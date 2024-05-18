f258d41e45f7df07ff13f5453f6460331d38cd41
Mariusz Bernacki <consulting@didalgo.com>
2024-05-18 02:35:24 +0200

    Remove dead code

--- src/main/java/com/didalgo/intellij/chatgpt/core/StartUpActivity.java
+++ src/main/java/com/didalgo/intellij/chatgpt/core/StartUpActivity.java
@@ -4,12 +4,6 @@
  */
 package com.didalgo.intellij.chatgpt.core;
 
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManager;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.extensions.PluginId;
-import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurableEP;
 import com.intellij.openapi.project.Project;
 import com.didalgo.intellij.chatgpt.settings.ChatGptSettings;
 import com.didalgo.intellij.chatgpt.ui.action.editor.ActionsUtil;
@@ -22,27 +16,5 @@ public class StartUpActivity implements StartupActivity {
     public void runActivity(@NotNull Project project) {
         ChatGptSettings.getInstance();
         ActionsUtil.refreshActions();
-        //Incubator.registerApplicationConfigurables();
-    }
-
-    static class Incubator {
-
-        //         <applicationConfigurable parentId="com.didalgo.chatgpt.settings.OpenAISettings" instance="com.didalgo.intellij.chatgpt.settings.GPT4_Panel"
-        //                                 id="com.didalgo.chatgpt.settings.GPT4"
-        //                                 displayName="GPT 4"/>
-        protected static void registerApplicationConfigurables() {
-            IdeaPluginDescriptor pluginDesc = PluginManager.getInstance().findEnabledPlugin(PluginId.getId("com.didalgo.chatgpt"));
-            ConfigurableEP<Configurable> bean = new ConfigurableEP<>(pluginDesc);
-            bean.parentId = "com.didalgo.chatgpt.settings.OpenAISettings";
-            bean.instanceClass = "com.didalgo.intellij.chatgpt.settings.GPT4o_Panel";
-            bean.id = "com.didalgo.chatgpt.settings.GPT4o";
-            bean.displayName = "GPT-4o";
-
-            ApplicationManager.getApplication().getExtensionArea()
-                    .getExtensionPoint(Configurable.APPLICATION_CONFIGURABLE)
-                    .registerExtension(bean);
-
-        }
-
     }
 }
