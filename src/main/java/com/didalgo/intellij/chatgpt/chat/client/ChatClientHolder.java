8e03bb1dbd010e27f70e1460532ff6578179626f
Mariusz Bernacki <consulting@didalgo.com>
2024-05-23 02:08:12 +0200

    Fix: properly shutdown `Disposable` chat clients

--- src/main/java/com/didalgo/intellij/chatgpt/chat/client/ChatClientHolder.java
+++ src/main/java/com/didalgo/intellij/chatgpt/chat/client/ChatClientHolder.java
@@ -5,13 +5,20 @@
 package com.didalgo.intellij.chatgpt.chat.client;
 
 import com.didalgo.intellij.chatgpt.chat.AssistantType;
+import com.didalgo.intellij.chatgpt.ui.MainConversationHandler;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.beans.factory.DisposableBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatClientHolder {
+
+    private static final Logger log = Logger.getInstance(MainConversationHandler.class);
 
     private static final Map<AssistantType, ChatClient> chatClients = new ConcurrentHashMap<>();
 
@@ -21,7 +28,17 @@ public class ChatClientHolder {
     }
 
     public static synchronized void refresh() {
+        List<ChatClient> clients = new ArrayList<>(chatClients.values());
         chatClients.clear();
+        clients.forEach(client -> {
+            if (client instanceof DisposableBean disposable) {
+                try {
+                    disposable.destroy();
+                } catch (Exception e) {
+                    log.error(e);
+                }
+            }
+        });
     }
 
     protected static ChatClient createChatClient(AssistantType type) {
