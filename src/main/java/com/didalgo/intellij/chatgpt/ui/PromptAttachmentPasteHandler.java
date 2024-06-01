205249053ddc1140eef8c08cf8cf2575b230a1ae
Mariusz Bernacki <consulting@didalgo.com>
2024-06-01 02:19:10 +0200

    Speculative fix: Call original paste handler despite exceptions (#23)

--- src/main/java/com/didalgo/intellij/chatgpt/ui/PromptAttachmentPasteHandler.java
+++ src/main/java/com/didalgo/intellij/chatgpt/ui/PromptAttachmentPasteHandler.java
@@ -7,6 +7,7 @@ package com.didalgo.intellij.chatgpt.ui;
 import com.didalgo.intellij.chatgpt.ui.text.ExpandableTextComponentEditorImpl;
 import com.didalgo.intellij.chatgpt.ui.text.ExpandableTextFieldExt;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
@@ -19,6 +20,8 @@ import java.awt.datatransfer.Transferable;
 
 public class PromptAttachmentPasteHandler extends EditorActionHandler {
 
+    private static final Logger log = Logger.getInstance(PromptAttachmentPasteHandler.class);
+
     private final EditorActionHandler originalHandler;
 
     public PromptAttachmentPasteHandler(EditorActionHandler originalHandler) {
@@ -27,13 +30,17 @@ public class PromptAttachmentPasteHandler extends EditorActionHandler {
 
     @Override
     protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
-        if (editor.getClass() == ExpandableTextComponentEditorImpl.class) {
-            var handler = dataContext.getData(ExpandableTextFieldExt.PROMPT_ATTACHMENT_HANDLER_KEY);
-            if (handler != null) {
-                var content = getContentsToPasteToEditor(null);
-                if (content != null && handler.handleTransferable(content))
-                    return;
+        try {
+            if (editor.getClass() == ExpandableTextComponentEditorImpl.class) {
+                var handler = dataContext.getData(ExpandableTextFieldExt.PROMPT_ATTACHMENT_HANDLER_KEY);
+                if (handler != null) {
+                    var content = getContentsToPasteToEditor(null);
+                    if (content != null && handler.handleTransferable(content))
+                        return;
+                }
             }
+        } catch (Exception e) {
+            log.error("Unexpected error occurred while handling pasted content", e);
         }
         originalHandler.execute(editor, caret, dataContext);
     }
