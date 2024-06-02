924f8ef8898aac2905cf3999a96d2d406c8cbcd6
Mariusz Bernacki <consulting@didalgo.com>
2024-06-02 02:37:30 +0200

    Fix: broken paste action on some IDEs, e.g. Rider (#23)

--- src/main/java/com/didalgo/intellij/chatgpt/ui/PromptAttachmentPasteHandler.java
+++ src/main/java/com/didalgo/intellij/chatgpt/ui/PromptAttachmentPasteHandler.java
@@ -11,6 +11,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.editor.actionSystem.EditorTextInsertHandler;
 import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.util.Producer;
 import org.jetbrains.annotations.NotNull;
@@ -18,7 +19,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.awt.datatransfer.Transferable;
 
-public class PromptAttachmentPasteHandler extends EditorActionHandler {
+public class PromptAttachmentPasteHandler extends EditorActionHandler implements EditorTextInsertHandler {
 
     private static final Logger log = Logger.getInstance(PromptAttachmentPasteHandler.class);
 
@@ -30,19 +31,39 @@ public class PromptAttachmentPasteHandler extends EditorActionHandler {
 
     @Override
     protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
+        if (tryHandlePastedContent(editor, dataContext, null))
+            return;
+
+        originalHandler.execute(editor, caret, dataContext);
+    }
+
+    @Override
+    public void execute(Editor editor, DataContext dataContext, @Nullable Producer<? extends Transferable> producer) {
+        if (tryHandlePastedContent(editor, dataContext, null))
+            return;
+
+        if (originalHandler instanceof EditorTextInsertHandler orgInsrtHandler)
+            orgInsrtHandler.execute(editor, dataContext, producer);
+        else
+            originalHandler.execute(editor, (Caret)null, dataContext);
+    }
+
+    private boolean tryHandlePastedContent(@NotNull Editor editor,
+                                           DataContext dataContext,
+                                           @Nullable Producer<? extends Transferable> producer) {
         try {
             if (editor.getClass() == ExpandableTextComponentEditorImpl.class) {
                 var handler = dataContext.getData(ExpandableTextFieldExt.PROMPT_ATTACHMENT_HANDLER_KEY);
                 if (handler != null) {
-                    var content = getContentsToPasteToEditor(null);
+                    var content = getContentsToPasteToEditor(producer);
                     if (content != null && handler.handleTransferable(content))
-                        return;
+                        return true;
                 }
             }
         } catch (Exception e) {
             log.error("Unexpected error occurred while handling pasted content", e);
         }
-        originalHandler.execute(editor, caret, dataContext);
+        return false;
     }
 
     protected Transferable getContentsToPasteToEditor(@Nullable Producer<? extends Transferable> producer) {
