0e6082ab422c673697a0f4e69830700f93af0ebe
Mariusz Bernacki <consulting@didalgo.com>
2024-06-20 22:07:29 +0200

    Add support for Claude 3.5 Sonnet

--- src/main/java/com/didalgo/intellij/chatgpt/chat/models/StandardModel.java
+++ src/main/java/com/didalgo/intellij/chatgpt/chat/models/StandardModel.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public enum StandardModel implements ModelType {
+    CLAUDE_3_5_SONNET("claude-3-5-sonnet-20240620", ModelFamily.ANTHROPIC, 200000),
     CLAUDE_3_OPUS("claude-3-opus-20240229", ModelFamily.ANTHROPIC, 200000),
     CLAUDE_3_SONNET("claude-3-sonnet-20240229", ModelFamily.ANTHROPIC, 200000),
     CLAUDE_3_HAIKU("claude-3-haiku-20240307", ModelFamily.ANTHROPIC, 200000),
